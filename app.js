379e31b7394bb9775e6aff3694d22af31dd51be9
amosthibault19@gmail.com <amosthibault19@gmail.com>
2023-01-02 03:27:14 -0500

    BANK WEB PAGE

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('header');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +30,19 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+const message=document.createElement('div');
+message.classList.add('cookies-message');
+message.innerHTML ='we use cookies for improved functionality and performance. <button class="btn btn--close-cookies">Got it!</button>';
+header.append(message);
+message.style.backgroundColor='#37383d';
+message.style.width='105%';
+//message.style.height=Number.parseFloat(getComputedStyle(message.height)+40+px);
+message.style.textAlign='center';
+message.style.color='#ffff';
+document.querySelector('.btn--close-cookies').addEventListener('click', function(){
+  message.remove();
+})
+//changing a customized properties which was declared in the css file 
+//document.documentElement.style.setProperty('--color-primary','orangered');
+
+ 
