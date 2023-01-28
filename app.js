c6dafb2ea83e65e102aa5a78bb9b3b98dbfaa629
amosthibault19@gmail.com <amosthibault19@gmail.com>
2023-01-28 12:43:39 -0500

    Bank Prototype web page

--- app.js
+++ app.js
@@ -142,11 +142,19 @@ allSections.forEach(function(section){
 const imgTarget=document.querySelectorAll('img[data-src]');
 const loadImg=function(entries, observer){
 const [entry]=entries;
+if(!entry.isIntersecting)return;
+//replace src with data-src
+entry.target.src=entry.target.dataset.src;
 
+entry.target.addEventListener('load',function(){
+  entry.target.classList.remove('lazy-img')
+})
+observer.unobserve(entry.target);
 }
-const imgOberserver=new IntersectionObserver(loadImg,{
+const imgObserver=new IntersectionObserver(loadImg,{
 root: null,
 threshold:0,
+rootMargin: '-200px',
 })
-imgTarget.forEach(img=>imgOberserver.observe(img));
-
+imgTarget.forEach(img=>imgObserver.observe(img))
+const slides=document.querySelectorAll('.slide')
